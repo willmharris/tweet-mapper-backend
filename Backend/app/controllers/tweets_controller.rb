b2e7be26ce97621f0df9c6b52218450bc6294de0
willmharris <willmharris0@gmail.com>
2020-03-25 03:05:37 -0400

    got itertive hashtag functionality from the input to the rails controller, but not with rendering the chart yet

--- Backend/app/controllers/tweets_controller.rb
+++ Backend/app/controllers/tweets_controller.rb
@@ -1,30 +1,18 @@
 class TweetsController < ApplicationController
-
-    def index 
-        start_date = params[:start]
-        end_date = params[:end]
-        hashtag1 = params[:hashtag1]
-        hashtag2 = params[:hashtag2]
-        tweetsa1 = Tweet.all.select do |tweet|
-            tweet.hashtag == hashtag1
-        end
-        tweetsa2 = tweetsa1.select do |tweet|
-            start_date.to_i <= tweet.date.to_i && tweet.date.to_i <= end_date.to_i
+    def create   
+        # Get information from the parameters 
+        payload = params[:payload]
+        start_date = payload[:start].to_i
+        end_date = payload[:end].to_i 
+        # Iterate through each hashtag and find all the tweets that match it within the start and end dates 
+        hashtags_list = payload.except(:start, :end)
+        tweets_array = []
+        hashtags_list.each_value do |hashtag|
+            tweets_array << Tweet.all.select do |tweet| 
+                tweet.hashtag == hashtag && tweet.date.to_i > start_date && tweet.date.to_i < end_date
+            end 
         end 
-        tweetsb1 = Tweet.all.select do |tweet|
-            tweet.hashtag == hashtag2
-        end
-        tweetsb2 = tweetsb1.select do |tweet|
-            start_date.to_i <= tweet.date.to_i && tweet.date.to_i <= end_date.to_i
-        end 
-        results = [tweetsa2, tweetsb2]
-        render json: results
-    end 
-
-    def show 
-        tweet = Tweet.find(params[:id])
-        render json: tweet
-    end 
-
+        render json: tweets_array 
+    end
 end
 
