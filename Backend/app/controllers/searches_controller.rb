d3582b70f79fc719ba0a77bd54bfc9a47962f831
willmharris <willmharris0@gmail.com>
2020-03-25 20:54:33 -0400

    saved search display bug fixed - all functionality is perfect now

--- Backend/app/controllers/searches_controller.rb
+++ Backend/app/controllers/searches_controller.rb
@@ -1,7 +1,8 @@
 class SearchesController < ApplicationController
     
     def create 
-        Search.create(user_id: params[:user_id], hashtags: params[:hashtags])
+        new_search = Search.create(user_id: params[:user_id], hashtags: params[:hashtags], start_date: params[:start_date], end_date: params[:end_date])
+        render json: new_search
     end 
 
     def destroy
